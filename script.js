5178aeaade01cd088ddd4a00f21ff25372c0b4f0
Gilberto Martins <gilbertomartinspro@gmail.com>
2023-01-11 16:41:07 -0300

    Implementando a descriptografia de texto

--- script.js
+++ script.js
@@ -3,18 +3,53 @@ let noMessage = document.querySelector(".no-message");
 let result = document.querySelector(".result");
 let resultText = document.getElementById("resultText");
 let btnEncrypt = document.querySelector("#encrypt");
+let btnDecrypt = document.querySelector("#decrypt");
+
+function viewResult() {
+    noMessage.style.display = "none";
+    result.style.display = "flex";
+}
+
+function reset() {
+    noMessage.style.display = "flex";
+    result.style.display = "none";
+    informedText.focus();
+}
 
 function encrypt() {
 
-    noMessage.style.display = "none";
-    result.style.display = "flex";
+    viewResult();
 
     if(informedText.value != "") {
         informedText.value = informedText.value.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat");
+        resultText.textContent = informedText.value;
+        informedText.value = "";
+        informedText.focus();
+    } 
+    else {
+        alert("Informe um texto para ser criptografado.");
+        reset();
     }
-    resultText.textContent = informedText.value;
-    informedText.value = "";
     
 }
 
 btnEncrypt.onclick = encrypt;
+
+function decrypt() {
+
+    viewResult();
+
+    if(informedText.value != "") {
+        informedText.value = informedText.value.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
+        resultText.textContent = informedText.value;
+        informedText.value = "";
+        informedText.focus();
+    } 
+    else {
+        alert("Informe um texto para ser descriptografado.");
+        reset();
+    }
+    
+}
+
+btnDecrypt.onclick = decrypt;
